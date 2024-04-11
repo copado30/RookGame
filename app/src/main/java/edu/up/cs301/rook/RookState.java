c88e7ff0f4bc7e4e946370be62811fd39bde7c0f
rafael <rafaelcopado30@gmail.com>
2024-04-11 14:27:35 -0700

    Same code as before with fixed shuffle method

--- app/src/main/java/edu/up/cs301/rook/RookState.java
+++ app/src/main/java/edu/up/cs301/rook/RookState.java
@@ -97,8 +97,8 @@ public class RookState extends GameState {
             Random spot = new Random();
             //temp1 and temp 2 represent the indexes of the cards
             int num1 = spot.nextInt(41), num2 = spot.nextInt(41);
-            Card holderCard = new Card(deck[num1]);//create a copy of a card as a place holder
-            deck[num1] = new Card(deck[num2]);//swaps the cards
+            Card holderCard =  deck[num1];//create a copy of a card as a place holder
+            deck[num1] = deck[num2];//swaps the cards
             deck[num2] = holderCard;
         }
 
